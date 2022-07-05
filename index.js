2743c0ad869f5a4cfdf9379fbe5c51c1552075e7
Ifzy keys <johnsonnifemi8@gmail.com>
2022-07-05 11:52:36 +0100

    added script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import display from "./modules/display.js";
-import dateNow from "./modules/date.js";
-import navigation from "./modules/navigation.js";
+import display from './modules/display.js';
+import dateNow from './modules/date.js';
+import navigation from './modules/navigation.js';
 
 dateNow();
 display();
